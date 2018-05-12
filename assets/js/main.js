f7a5d5b0dee182f74a178855f3bd1d44af78e69f
Junho Park <cnaa97@gmail.com>
2018-05-12 23:07:02 +0900

    apply script, event, change css

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,8 @@ $(function(){
     
 
     var $root = $('html, body');
+    var $header = $('#header');
+
     /* smooth scroll */ 
     var $navLinks = $('#header nav li a');
     $navLinks.click(function(){
@@ -18,9 +20,21 @@ $(function(){
         }, 450);
     })
 
+    // scroll event
+    $(window).on('scroll', function(){
+        var scrollTop = $root.scrollTop();
+        var aboutTop = $('.section-about').offset().top;
+
+        if (scrollTop && scrollTop > (aboutTop - 100) ) {
+            $header.addClass('scroll');
+        } else {
+            $header.hasClass('scroll') && $header.removeClass('scroll');
+        }
+    })
+
     /* scroll event */
     $('.section').Oppear({
-        transition : '1.5s',
+        transition : '1.2s',
     });
 
     /* init tab menu content */
@@ -46,88 +60,31 @@ $(function(){
         $(this).toggleClass('active');
     });
 
-	/* ICO 모금액 */
-	// $('#myGoal').stepProgressBar({
-	// 	currentValue: 200,
-	// 	steps: [
-	// 		{ value: 100 }, {
-	// 		value: 500,
-	// 		bottomLabel: '<i class="material-icons">프라이빗판매</i>'
-	// 		}, {
-	// 			value: 800,
-	// 			bottomLabel: '<i class="material-icons">소프트캡</i>'
-	// 		}, {  
-	// 			value: 1000, 
-	// 			bottomLabel: '<i class="material-icons">하드캡</i>',
-	// 			mouseOver: function() { alert('mouseOver'); },
-	// 			click: function() { alert('click'); }
-	// 	}],
-	// 	unit: 'ETH'
-	// });
-	
-	/* ICO 시작일 */
-	// $("#getting-started")
-	// 	.countdown("2017/01/01", function(event) {
-	// 	$(this).text(
-	// 		event.strftime('%D days %H:%M:%S')
-	// 	);
-    // });
-    
+    /* mobile menu */
+    $('.mobile-nav').click(function(){
+        $(this).toggleClass('active');
+        $('#header .logo').toggleClass('active');
+        $('#header nav').toggleClass('active');
+    })
 
-	var start = 0;
+    $('#header > nav > ul > li > a').click(function(){
+        $('.mobile-nav').removeClass('active');
+        $('#header .logo').removeClass('active');
+        $('#header nav').removeClass('active');
+    })
+	
+    /* count down */
     var end = Math.floor(new Date('6/4/2018').getTime() / 1000);
     var now = Math.floor(new Date().getTime() / 1000);
     
 	$('#countdown').final_countdown({
-        start: start,
+        start: 0,
         end: end,
         now: now,
-        seconds: {
-            borderColor: '#fff',
-            borderWidth: '4'
-        },
-        minutes: {
-            borderColor: '#fff',
-            borderWidth: '4'
-        },
-        hours: {
-            borderColor: '#fff',
-            borderWidth: '4'
-        },
-        days: {
-            borderColor: '#fff',
-            borderWidth: '4'
-        },
+        seconds: { borderColor: '#fff', borderWidth: '4' },
+        minutes: { borderColor: '#fff', borderWidth: '4' },
+        hours: { borderColor: '#fff', borderWidth: '4' },
+        days: { borderColor: '#fff', borderWidth: '4' },
     });
 	
-    /* Chart */
-    var ctx = $("#myChart");
-    var myPieChart = new Chart(ctx,{
-        type: 'pie',
-        data: {
-            label: '# of Votes',
-            datasets: [{
-                data: [40, 20, 10, 30],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)'
-                ],
-                borderWidth: 0
-            }],
-            labels: [
-                'Contributor', 'Yellow'
-            ],
-        },
-        // options: {
-        //     legend: {
-        //         display: true,
-        //         labels: {
-        //             fontColor: 'rgb(255, 99, 132)'
-        //         }
-        //     }
-        // }
-    });
-
 });
