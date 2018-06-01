b6d1fd9d3caff1aac2bf483cc357e6035aca6a57
Junho Park <cnaa97@gmail.com>
2018-06-01 10:36:21 +0900

    apply open graph

--- ko.php
+++ ko.php
@@ -9,6 +9,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>GROO Coin</title>
+    <meta property="og:title" content="GROO Coin" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://groo.io/" />
+    <meta property="og:image" content="https://groo.io/assets/images/open-graph.png" />
+    <meta property="og:description" content="Beauty Content Social Media. Connect & Share your Billions of Grooming knowledge." />
+    <meta property="og:locale" content="ko_KR" />
+
     <link rel="stylesheet" href="dist/reset.css">
     <link rel="stylesheet" href="dist/style.css">
     <link rel="stylesheet" href="dist/style.media.query.css">
