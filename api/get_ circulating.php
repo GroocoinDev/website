8e388eebb9c0d549554de7ea07782635f96340ad
jkinject <jkinject@gmail.com>
2018-12-17 14:11:45 +0900

    api 추가

---
+++ api/get_ circulating.php
@@ -0,0 +1,10 @@
+<?php
+    $type = $_GET['type'];
+
+    if($type == "1") {
+        echo number_format(521658658.54053, 18);
+    } else {
+        echo "521658658540530000000000000";
+    }
+
+?>
