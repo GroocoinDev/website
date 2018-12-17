ca4e66c642f41fab399d20fa0b473c210a789e26
jkinject <jkinject@gmail.com>
2018-12-17 14:18:17 +0900

    api 추가

--- api/get_circulating.php
+++ api/get_circulating.php
@@ -2,9 +2,9 @@
     $type = $_GET['type'];
 
     if($type == "1") {
-        echo number_format(521658658.54053, 18);
+        echo number_format(521658658.54053, 18)." GROO";
     } else {
-        echo "521658658540530000000000000";
+        echo "521658658540530000000000000 GROO";
     }
 
 ?>
