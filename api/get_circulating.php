d328f13796f71b532ff6c21d193e7868b276cfa0
jkinject <jkinject@gmail.com>
2018-12-17 14:19:09 +0900

    api 변경

--- api/get_circulating.php
+++ api/get_circulating.php
@@ -2,7 +2,7 @@
     $type = $_GET['type'];
 
     if($type == "1") {
-        echo number_format(521658658.54053, 18)." GROO";
+        echo number_format(521658658.540530000000000000, 18)." GROO";
     } else {
         echo "521658658540530000000000000 GROO";
     }
