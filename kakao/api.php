13285bbf80ffab37dd6839db1eba172e57c625f3
jkinject <jkinject@gmail.com>
2018-12-28 00:59:30 +0900

    카카오봇 테스트

--- kakao/api.php
+++ kakao/api.php
@@ -1,5 +1,14 @@
 <?php
 	header("Content-Type: application/json;charset=utf-8");
+
+	$json1 = file_get_contents('http://api.coinmarketcap.com/v1/ticker/ethereum/?convert=KRW');
+    $data1 = json_decode($json1, true);
+	$eth_price = $data1[0]['price_krw'];
+	
+	$json2 = file_get_contents('http://app.stex.com/api2/trades?pair=GROO_ETH');
+    $data2 = json_decode($json2, true);
+	$groo_price = $data2['result'][0]['price'];
+    $groo_price_krw = $groo_price * $eth_price;
 ?>
 
 {
@@ -8,18 +17,18 @@
 		"outputs" : [{
 			"basicCard": {
 				"title": "그루코인 실시간 가격",
-				"description": "1 GROO = 0.00001290 ETH\n1 GROO = 1.6 원",
+				"description": "1 GROO = <?=$groo_price?> ETH\n1 GROO = <?=$groo_price_krw?> 원",
 				"thumbnail": {
 					"imageUrl": "https://groo.io/assets/img/open-graph.png?v=1"
 				},
 				"buttons": [
 					{
-						"action": "",
+						"action": "webLink",
 						"label": "그루코인 가격차트",
-						"webLinkUrl": "https://e.kakao.com/t/hello-ryan"
+						"webLinkUrl": "https://www.coingecko.com/en/price_charts/groocoin/krw"
 					},
 					{
-						"action": "",
+						"action": "webLink",
 						"label": "그루코인 거래하기",
 						"webLinkUrl": "https://app.stex.com/en/trade/pair/ETH/GROO/30"
 					}
