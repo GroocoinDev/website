dd1801d6e3d5af411127d7bef5cca3654d100fe4
jkinject <jkinject@gmail.com>
2018-12-28 00:00:39 +0900

    카카오봇 테스트

--- kakao/api.php
+++ kakao/api.php
@@ -2,4 +2,13 @@
 	header("Content-Type: application/json;charset=utf-8");
 ?>
 
-{"version":"2.0","template":{"outputs":[{"simpleText":{"text":"hello I'm Ryan"}}]}}
+{
+	"version" : "2.0",
+	"template" : {
+		"outputs" : [{
+			"simpleText" : {
+				"text" : "[그루코인 가격]\n1 GROO = 0.00001290 ETH\n1 GROO = 1.6원"
+			}
+		}]
+	}
+}
