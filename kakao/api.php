829ebe3e6686e5250715d3113d97a4cbed74028f
jkinject <jkinject@gmail.com>
2018-12-27 23:36:22 +0900

    카카오봇 테스트

--- kakao/api.php
+++ kakao/api.php
@@ -2,4 +2,4 @@
 	header("Content-Type: application/json;charset=utf-8");
 ?>
 
-{"version":"2.0","template":{"outputs":[{"simpleText":{"text":"hello I'm Ryan"}}]}
+{"version":"2.0","template":{"outputs":[{"simpleText":{"text":"hello I'm Ryan"}}]}}
