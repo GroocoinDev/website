fcd59630ee6d42d26b665119bb265c41ffaa7852
jkinject <jkinject@gmail.com>
2018-12-28 00:41:34 +0900

    카카오봇 테스트

--- kakao/api.php
+++ kakao/api.php
@@ -6,8 +6,24 @@
 	"version" : "2.0",
 	"template" : {
 		"outputs" : [{
-			"simpleText" : {
-				"text" : "[그루코인 가격]\n1 GROO = 0.00001290 ETH\n1 GROO = 1.6원"
+			"basicCard": {
+				"title": "그루코인 실시간 가격",
+				"description": "1 GROO = 0.00001290 ETH\n1 GROO = 1.6 원",
+				"thumbnail": {
+					"imageUrl": "https://groo.io/assets/img/open-graph.png?v=1"
+				},
+				"buttons": [
+					{
+						"action": "",
+						"label": "그루코인 가격차트",
+						"webLinkUrl": "https://e.kakao.com/t/hello-ryan"
+					},
+					{
+						"action": "",
+						"label": "그루코인 거래하기",
+						"webLinkUrl": "https://app.stex.com/en/trade/pair/ETH/GROO/30"
+					}
+				]
 			}
 		}]
 	}
