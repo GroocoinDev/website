428b1503035303756c8a068a741b77c7bfc806db
jkinject <jk.jang@epcompany.co.kr>
2019-05-27 16:11:14 +0900

    init

---
+++ game/index.php
@@ -0,0 +1,20 @@
+<p>GROO GAME</p>
+
+<ul>
+    <li>Dice</li>
+    <li>Roulette</li>
+    <li>Blackjack</li>
+</ul>
+
+<ul>
+    <li>Bet GROO : 5,000,000</li>
+    <li>User WIN GROO : 1,000,000</li>
+    <li>User LOSE GROO : 4,000,000</li>
+</ul>
+
+<button>Login</button>
+
+<p>Game History</p>
+<div>
+    
+</div>
